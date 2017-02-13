1c90ef17eb1ddb241c8226f9b93a4dd81e073cfa
John Slee <john.slee@fairfaxmedia.com.au>
2017-02-13 20:36:56 +1100

    add audit_all command

--- lib/epaudit/cli.rb
+++ lib/epaudit/cli.rb
@@ -21,12 +21,34 @@ module EPAudit
         $logger.info results[check].inspect
       end
 
-      renderer = Renderer.new(
-        :results      => results,
-        :template_dir => auditor.config.dig('epaudit','template_dir'),
-      )
+      renderer = Renderer.new(:template_dir => auditor.config.dig('epaudit','template_dir'))
 
-      pp renderer.render
+      pp renderer.render(results)
+    end
+
+    desc 'audit_all', 'audit all configured endpoints'
+    def audit_all
+      auditor = Auditor.new
+      endpoints = [ auditor.config.dig('epaudit','endpoints').flatten ]
+      if endpoints.size < 1
+        $logger.error("must supply endpoints")
+        exit false
+      end
+
+      result_sets = {}
+      endpoints.flatten.each do |endpoint|
+        $logger.debug "auditing: #{endpoint}"
+        result_sets[endpoint] = auditor.audit(endpoint)
+      end
+
+      renderer = Renderer.new(:template_dir => auditor.config.dig('epaudit','template_dir'))
+
+      rendering_sets = {}
+      result_sets.each_pair do |endpoint,results|
+        rendering_sets[endpoint] = renderer.render(results)
+      end
+
+      pp rendering_sets
     end
   end
 end
